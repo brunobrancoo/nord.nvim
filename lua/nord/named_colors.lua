168c7628f536992806348b074ef26591bf8a9550
Bruno Camargos Branco <brunobranco@iusevimbtw.com>
2026-04-23 16:34:03 -0300

    Update named_colors.lua

--- lua/nord/named_colors.lua
+++ lua/nord/named_colors.lua
@@ -1,23 +1,25 @@
 local nord = {
-	black = "#342C31",
-	dark_gray = "#40363C",
-	gray = "#4B4047",
-	light_gray = "#5B4E56",
-	light_gray_bright = "#74636C",
+	-- Base vinho
+	black = "#1A1317",
+	dark_gray = "#241A20",
+	gray = "#32262E",
+	light_gray = "#44363F",
+	light_gray_bright = "#5C4A55",
 
-	darkest_white = "#D8CACE",
-	darker_white = "#E2D6D9",
-	white = "#EADFE2",
+	darkest_white = "#D0C4C8",
+	darker_white = "#DCD0D4",
+	white = "#E8DEE0",
 
-	teal = "#A9BFAF",
-	off_blue = "#D8A0A8",
-	glacier = "#C97E88",
-	blue = "#B45A66",
-	red = "#BF616A",
-	orange = "#D08770",
-	yellow = "#EBCB8B",
-	green = "#A3BE8C",
-	purple = "#B79AAF",
+	-- Acentos verde (slots compatíveis com o layout Nord original)
+	teal = "#8FBC8B",
+	off_blue = "#A8C4A5",
+	glacier = "#6B9B78",
+	blue = "#4F8A62",
+	red = "#C16B72",
+	orange = "#D08A5E",
+	yellow = "#DCC27D",
+	green = "#9ED4A0",
+	purple = "#9B8AB5",
 	none = "NONE",
 }
 
